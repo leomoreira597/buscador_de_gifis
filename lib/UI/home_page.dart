525c22ce0491776bcdac701fd63af7fd9e1032a2
Leonardo Moreira <lmoreira597@gmail.com>
2021-11-23 08:44:33 -0300

    inicio buscador de gifs

---
+++ lib/UI/home_page.dart
@@ -0,0 +1,121 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String _search = "";
+  int _offset = 0;
+
+  Future<Map> _getGifs() async {
+    http.Response response;
+    if (_search == null) {
+      response = await http.get(Uri.parse(
+          "https://api.giphy.com/v1/gifs/trending?api_key=NTqDH17uwwCjWcw7mQiirGGQ1dk6ap5t&limit=20&rating=g"));
+      throw '';
+    } else {
+      response = await http.get(Uri.parse(
+          "https://api.giphy.com/v1/gifs/search?api_key=NTqDH17uwwCjWcw7mQiirGGQ1dk6ap5t&q=$_search&limit=25&offset=$_offset&rating=g&lang=pt"));
+      return json.decode(response.body);
+    }
+  }
+
+  @override
+  void iniState() {
+    super.initState();
+    _getGifs().then((map) {
+      print(map);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Image.network(
+            "https://developers.giphy.com/branch/master/static/header-logo-0fec0225d189bc0eae27dac3e3770582.gif"),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: "Pesquise Aqui",
+                labelStyle: TextStyle(color: Colors.white),
+                border: OutlineInputBorder(),
+              ),
+              style: TextStyle(color: Colors.white, fontSize: 10.0),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Expanded(
+            child: FutureBuilder(
+                future: _getGifs(),
+                builder: (context, snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.waiting:
+                    case ConnectionState.none:
+                      return Container(
+                        width: 200.0,
+                        height: 200.0,
+                        alignment: Alignment.center,
+                        child: CircularProgressIndicator(
+                          valueColor:
+                              AlwaysStoppedAnimation<Color>(Colors.white),
+                          strokeWidth: 5.0,
+                        ),
+                      );
+                    default:
+                      if (snapshot.hasError) {
+                        return Container(
+                          child:  TextField(
+                            decoration: InputDecoration(
+                              labelText: "Pesquise Aqui",
+                              labelStyle: TextStyle(color: Colors.white),
+                              border: OutlineInputBorder(),
+                            ),
+                            style: TextStyle(color: Colors.white, fontSize: 10.0),
+                            textAlign: TextAlign.center,
+                          ),
+                        );
+                      } else {
+                        return _criarTabelaGif(context, snapshot);
+                      }
+                  }
+                }),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _criarTabelaGif(BuildContext context, AsyncSnapshot snapshot) {
+    return GridView.builder(
+        padding: EdgeInsets.all(10.0),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10.0,
+          mainAxisSpacing: 10.0,
+        ),
+        itemCount: snapshot.data["data"].length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            child: Image.network(
+              snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+              height: 300.0,
+              fit: BoxFit.cover,
+            ),
+          );
+        });
+  }
+}
